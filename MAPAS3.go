c84eaa6c4d0f69b25118e340d5a4cbae2ef0ba5e
LÍVIA LEMOS <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2023-06-17 20:56:03 -0300

    SOLUÇÃO 3

---
+++ MAPAS3.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	map1 := map[string]int{
+		"val1": 11,
+		"val2": 22,
+		"val3": 22,
+	}
+	map2 := map[string]int{
+		"val3": 11,
+		"val4": 44,
+	}
+	mapfinal := mapasjuntos(map1, map2)
+	fmt.Println("Seu mapa:", mapfinal)
+}
+
+func mapasjuntos(map1 map[string]int, map2 map[string]int) map[string]int {
+	final := make(map[string]int)
+	for var1, range1 := range map1 {
+		final[var1] = range1
+	}
+	for var2, range2 := range map2 {
+		final[var2] = final[var2] + range2
+	}
+	return final
+}
